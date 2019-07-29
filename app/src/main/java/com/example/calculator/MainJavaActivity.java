45d1b061590d96bdf2db45d2612ef2f6c8256ea4
achumphrey <achumphrey2002@yahoo.com>
2019-07-29 10:39:49 +0100

    The calculator challenge13

--- app/src/main/java/com/example/calculator/MainJavaActivity.java
+++ app/src/main/java/com/example/calculator/MainJavaActivity.java
@@ -3,12 +3,15 @@ package com.example.calculator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainJavaActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,6 +28,8 @@ public class MainJavaActivity extends AppCompatActivity {
         Button btnMultiply = findViewById(R.id.btn_multiply);
         Button btnDivide = findViewById(R.id.btn_divide);
         Button btnSubtract = findViewById(R.id.btn_subtract);
+
+        Log.i(TAG, "onCreate: ");
 
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -165,6 +170,42 @@ public class MainJavaActivity extends AppCompatActivity {
                 tvResult.setText(result);
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onReStart: ");
     }
 }
 
